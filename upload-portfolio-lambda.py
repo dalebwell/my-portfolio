0357ff20cc40ad79f6556f90e7153efac44dbccd
rdbwell <dalebwell@gmail.com>
2019-11-20 18:19:19 -0500

    Add SNS notification to deploy lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,37 +5,51 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-
+    
+    sns = boto3.resource('sns')
+    topic=sns.Topic('arn:aws:sns:us-east-1:545602794464:deployPortfolioTopic')
+    
     location = {
-        "bucketName": 'portfoliobuild.solocollab.online',
-        "objectKey": 'portfoliobuild.zip'
+            "bucketName": 'portfoliobuild.solocollab.online',
+            "objectKey": 'portfoliobuild.zip'
     }
-    job = event.get("CodePipeline.job")
-    
-    if job:
-        for artifact in job["data"]["inputArtifacts"]:
-            if artifact["name"] == "BuildArtifact":
-                location = artifact["location"]["s3Location"]
-                
-    print "Building portfolio from " + str(location)
-    
-    s3 = boto3.resource('s3')
 
-    portfolio_bucket = s3.Bucket('portfolio.solocollab.online')
-    build_bucket = s3.Bucket(location["bucketName"])
-
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+                    
+        print "Building portfolio from " + str(location)
+        
+        s3 = boto3.resource('s3')
     
-    print "Job done"
+        portfolio_bucket = s3.Bucket('portfolio.solocollab.online')
+        build_bucket = s3.Bucket(location["bucketName"])
     
-    if job:
-        codepipeline = boto3.client('codepipeline')
-        codepipeline.put_job_success_result(jobId=job["id"])
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+    
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        print "Job done"
+        topic.publish(Subject="Portfolio Update", Message="Portfolio Deployed Successfully")
+    
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+            
+    except:
+         topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio Not Deployed Successfully")
+         raise
+     
+     return "Hello From Lambda"
+        
